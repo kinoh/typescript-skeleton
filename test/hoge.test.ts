ecf05ae7be3f7ff5159d4fdf92d3e476772cc47f
Kino <p@kinon.sakura.ne.jp>
2017-10-14 22:37:20 +0900

    Test with mocha

---
+++ test/hoge.test.ts
@@ -0,0 +1,12 @@
+import assert from "power-assert";
+import { Hoge } from "../src/hoge";
+
+describe("Hoge", () => {
+	var hoge = new Hoge();
+
+	describe("#double", () => {
+		it("doubled", () => {
+			assert(hoge.double(123) === 246);
+		});
+	});
+});
